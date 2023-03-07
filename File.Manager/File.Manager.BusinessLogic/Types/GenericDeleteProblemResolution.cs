c51c950a5237d49e1cc50d0c51399b7ac854612e
Wojciech Sura <wojciechsura@gmail.com>
2023-03-07 15:56:23 +0100

    Deleting files in progress

--- File.Manager/File.Manager.BusinessLogic/Types/GenericDeleteProblemResolution.cs
+++ File.Manager/File.Manager.BusinessLogic/Types/GenericDeleteProblemResolution.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using BindingEnums;
+using File.Manager.BusinessLogic.TypeConverters;
+using File.Manager.Resources.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace File.Manager.BusinessLogic.Types
 {
-    internal class GenericDeleteProblemResolution
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
+    public enum GenericDeleteProblemResolution
     {
+        [LocalizedDescription(nameof(Strings.GenericDeleteProblemResolution_Ask), typeof(Strings))]
+        Ask,
+        [LocalizedDescription(nameof(Strings.GenericDeleteProblemResolution_Delete), typeof(Strings))]
+        Delete,
+        [LocalizedDescription(nameof(Strings.GenericDeleteProblemResolution_Skip), typeof(Strings))]
+        Skip,
+        [LocalizedDescription(nameof(Strings.GenericProblemResolution_Abort), typeof(Strings))]
+        Abort
     }
 }
